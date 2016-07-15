44294eb1735f1af25bfac1750fae6a8bf8da459b
Sohail Choudhary <bash_edu@outlook.com>
2016-07-15 14:58:05 +0530

    Create assignment implemented

---
+++ src/test/java/com/inportia/utils/reporting/EmailManager.java
@@ -0,0 +1,28 @@
+package com.inportia.utils.reporting;
+
+
+/* 
+ *  Use gmail SMTP
+ * */
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+public class EmailManager {
+    
+	   public static void sendMail()
+	   {
+		   
+	   }
+	   
+	   public static void sendMail(String subject,String content,String[] attachementList)
+	   {
+		   
+	   }
+}
